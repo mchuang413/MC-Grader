2103331eb0d9c533cfcc82ff7ef33a8a217b9731
komorebii <christopherzhang1111@gmail.com>
2025-02-13 22:14:11 -0800

    migration to gemini

--- routes/grade.js
+++ routes/grade.js
@@ -1,44 +1,44 @@
-import express from 'express';
-import fileUpload from 'express-fileupload';
-import {autograde} from '../utils/ai.js';
-import fs from 'fs';
-import path from 'path';
+import express from "express";
+import fileUpload from "express-fileupload";
+import { autograde } from "../utils/ai.js";
+import fs from "fs";
+import path from "path";
 
 const router = express.Router();
 router.use(fileUpload());
 
 const __dirname = import.meta.dirname;
-router.post('/', async (req, res) => {
-    const {lab} = req.body;
-    const files = req.files.doc;
-    let text = "";
-    if (Array.isArray(files)) {
-        files.forEach(file => {
-            text += `NOT PART OF CODE - File: ${file.name}\n`;
-            text += file.data.toString() + '\n\n';
-        });
-    } else {
-        text += `NOT PART OF CODE - File: ${files.name}\n`;
-        text += files.data.toString() + '\n\n';
-    }
-    console.log(text);
-    // console.log('File received:', file.name);
-    // console.log('Lab recieved:', lab);
-    //const text = file.data.toString();
-    const answer = await autograde(text, lab);
-    console.log(answer);
-    res.json(answer);
+router.post("/", async (req, res) => {
+  const { lab } = req.body;
+  const files = req.files.doc;
+  let text = "";
+  if (Array.isArray(files)) {
+    files.forEach((file) => {
+      text += `NOT PART OF CODE - File: ${file.name}\n`;
+      text += file.data.toString() + "\n\n";
+    });
+  } else {
+    text += `NOT PART OF CODE - File: ${files.name}\n`;
+    text += files.data.toString() + "\n\n";
+  }
+  console.log(text);
+  // console.log('File received:', file.name);
+  // console.log('Lab recieved:', lab);
+  //const text = file.data.toString();
+  const answer = await autograde(text, lab);
+
+  console.log(answer);
+  res.json(answer);
 });
 
-router.get('/labs', async (req, res) => {
-    const labsDir = path.join(__dirname, '..', 'labs');
-    const files = fs.readdirSync(labsDir);
-    const labNames = [];
-    for(const file of files){
-        labNames.push(path.parse(file).name); //Remove .txt extension
-    }
-    res.send(labNames);
-})
-
+router.get("/labs", async (req, res) => {
+  const labsDir = path.join(__dirname, "..", "labs");
+  const files = fs.readdirSync(labsDir);
+  const labNames = [];
+  for (const file of files) {
+    labNames.push(path.parse(file).name); //Remove .txt   extension
+  }
+  res.send(labNames);
+});
 
 export default router;
--- utils/ai.js
+++ utils/ai.js
@@ -1,45 +1,82 @@
-import OpenAI from 'openai';
-import dotenv from 'dotenv';
-import fs from 'fs';
-import path from 'path';
+// import OpenAI from 'openai';
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
 
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+//const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 const __dirname = import.meta.dirname;
 
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
 function readFile(path) {
-    return fs.readFileSync(path, 'utf-8');
+  return fs.readFileSync(path, "utf-8");
 }
 
 export async function autograde(content, lab) {
-    const systemPrompt = readFile(path.join(__dirname, 'system-prompt.txt'));
-    const labInfo = readFile(path.join(__dirname, '..', 'labs', `${lab}.txt`));
+  const systemPrompt = readFile(path.join(__dirname, "system-prompt.txt"));
+  const labInfo = readFile(path.join(__dirname, "..", "labs", `${lab}.txt`));
 
-    //console.log(labInfo);
+  const chatSession = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [
+          {
+            text: `${systemPrompt}\n\n-- START OF LAB INSTRUCTIONS: --\n${labInfo}\n-- END OF LAB INSTRUCTIONS: --`,
+          },
+        ],
+      },
+    ],
+  });
 
-    const completion = await openai.chat.completions.create({
-        model: 'gpt-4o-mini',
-        messages: [
-            { role: 'system', content: systemPrompt},
-            { role: 'system', content: `-- START OF LAB INSTRUCTIONS: --\n${labInfo}\n-- END OF LAB INSTRUCTIONS: --`},
-            {
-                role: 'user',
-                content: `-- START OF STUDENT CODE --\n${content}\n-- END OF STUDENT CODE --`,
-            }
-        ]
-    });
-    const message = completion.choices[0].message.content;
-    console.log(completion.choices[0].message.content);
+  const result = await chatSession.sendMessage(
+    `-- START OF STUDENT CODE --\n${content}\n-- END OF STUDENT CODE --`,
+  );
 
-    //Parse data
-    const lines = message.split('\n'); //array of lines
-    const score = parseFloat(lines[0]) || 0; //TODO: add input filtering
-    let comments = "";
-    for (let i = 1; i < lines.length; i++) {
-        if(i != 1) comments += '\n';
-        comments += lines[i];
-    }
-    return {score, comments};
+  const message = result.response.text();
+  //console.log(labInfo);
+
+  //const completion = await openai.chat.completions.create({
+  //   model: "gpt-4o-mini",
+  //   messages: [
+  //     { role: "system", content: systemPrompt },
+  //     {
+  //       role: "system",
+  //       content: `-- START OF LAB INSTRUCTIONS: --\n${labInfo}\n-- END OF LAB INSTRUCTIONS: --`,
+  //     },
+  //     {
+  //       role: "user",
+  //       content: `-- START OF STUDENT CODE --\n${content}\n-- END OF STUDENT CODE --`,
+  //     },
+  //   ],
+  // });
+  // const message = completion.choices[0].message.content;
+  // console.log(completion.choices[0].message.content);
+
+  //Parse data
+  const lines = message.split("\n"); //array of lines
+  const score = parseFloat(lines[0]) || 0; //TODO: add input filtering
+  let comments = "";
+  for (let i = 1; i < lines.length; i++) {
+    if (i != 1) comments += "\n";
+    comments += lines[i];
+  }
+  return { score, comments };
 }
